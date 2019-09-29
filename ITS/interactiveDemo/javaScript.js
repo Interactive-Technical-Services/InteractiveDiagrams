c8b7a0c0c961881c47b53235b2a756e950161a70
RDMay <[Interactive.Technical.Services@gmail.com]>
2019-09-29 17:24:00 -0400

    J

--- ITS/interactiveDemo/javaScript.js
+++ ITS/interactiveDemo/javaScript.js
@@ -47,12 +47,12 @@ var schematicDrag = Draggable.create(schematic, {zIndexBoost:false});
 schematic.addEventListener("DOMMouseScroll", function(e){zoomSchematic(e)}, false);
 schematic.addEventListener('gestureend', function(e) {
     if (e.scale < 1.0) {
-        alert('less than + e')
+        alert('lower than + e')
     } else if (e.scale > 1.0) {
-        alert('greater than' + e)
+        alert('higher than' + e)
     }
 }, false); 
- alert("rob")   
+ alert("pam")   
 
 var scaleUp = 1;
 function zoomSchematic(e){
