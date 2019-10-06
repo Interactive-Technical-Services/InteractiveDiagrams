9db406ab5ed78d13068c44d246aa8eb65e1ecbc6
RDMay <[Interactive.Technical.Services@gmail.com]>
2019-10-06 08:25:07 -0400

    remove wire code

--- ITS/hvacDemo/javaScript.js
+++ ITS/hvacDemo/javaScript.js
@@ -12,20 +12,20 @@ for(i=0; i<diagram1PathsLength; i++){
 	diagram1Paths[i].style['stroke-linecap']="round";
 	diagram1Paths[i].style.stroke = "Black";
 
-	var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-	path.setAttribute('stroke','blue');
-	path.setAttribute('fill','none');
-	path.setAttribute('opacity',0);
-	path.setAttribute('id',diagram1Paths[i].id + 'copy');
-	path.setAttribute('onclick','wireClicked(this);');
-	path.setAttribute('onmouseover','this.style.cursor = "default"; overPath(this);');
-	path.setAttribute('onmouseout','notOverPath(this);');
-	path.style['stroke-width']=1;
-	path.style['stroke-linecap']="round";
-	path.setAttribute("d", diagram1Paths[i].getAttribute("d"));
-	diagram1.appendChild(path);
-	path.style["stroke-width"]= 3;
-	path.setAttribute('touchmove','wireClicked(this);');	
+	// var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+	// path.setAttribute('stroke','blue');
+	// path.setAttribute('fill','none');
+	// path.setAttribute('opacity',0);
+	// path.setAttribute('id',diagram1Paths[i].id + 'copy');
+	// path.setAttribute('onclick','wireClicked(this);');
+	// path.setAttribute('onmouseover','this.style.cursor = "default"; overPath(this);');
+	// path.setAttribute('onmouseout','notOverPath(this);');
+	// path.style['stroke-width']=1;
+	// path.style['stroke-linecap']="round";
+	// path.setAttribute("d", diagram1Paths[i].getAttribute("d"));
+	// diagram1.appendChild(path);
+	// path.style["stroke-width"]= 3;
+	// path.setAttribute('touchmove','wireClicked(this);');	
 }
 
 function overPath(wire){
@@ -80,119 +80,41 @@ function chromeMouseWheelEvent(e){
 
 
 
-hammerIt(document.getElementById("schematic")); 
 
 
-function hammerIt(elm) {
-    hammertime = new Hammer(elm, {});
-    hammertime.get('pinch').set({
-        enable: true
-    });
-    var posX = 0,
-        posY = 0,
-        scale = 1,
-        last_scale = 1,
-        last_posX = 0,
-        last_posY = 0,
-        max_pos_x = 0,
-        max_pos_y = 0,
-        transform = "",
-        el = elm;
 
-    hammertime.on('doubletap pan pinch panend pinchend', function(ev) {
-        if (ev.type == "doubletap") {
-            transform =
-                "translate3d(0, 0, 0) " +
-                "scale3d(2, 2, 1) ";
-            scale = 2;
-            last_scale = 2;
-            try {
-                if (window.getComputedStyle(el, null).getPropertyValue('-webkit-transform').toString() != "matrix(1, 0, 0, 1, 0, 0)") {
-                    transform =
-                        "translate3d(0, 0, 0) " +
-                        "scale3d(1, 1, 1) ";
-                    scale = 1;
-                    last_scale = 1;
-                }
-            } catch (err) {}
-            el.style.webkitTransform = transform;
-            transform = "";
-        }
+//Samsung Pinch Zoom
+    // mainPage.addEventListener("touchstart", function(ev){
+    //     	var l = ev.touches.length;
+    //     	if (l > 1) { 
 
-        //pan    
-        if (scale != 1) {
-            posX = last_posX + ev.deltaX;
-            posY = last_posY + ev.deltaY;
-            max_pos_x = Math.ceil((scale - 1) * el.clientWidth / 2);
-            max_pos_y = Math.ceil((scale - 1) * el.clientHeight / 2);
-            if (posX > max_pos_x) {
-                posX = max_pos_x;
-            }
-            if (posX < -max_pos_x) {
-                posX = -max_pos_x;
-            }
-            if (posY > max_pos_y) {
-                posY = max_pos_y;
-            }
-            if (posY < -max_pos_y) {
-                posY = -max_pos_y;
-            }
-        }
+schematic.addEventListener('gesturechange', function(e) {
+	e.preventDefault();
+    if (e.scale < 1.0 && scaleUp > 1) {
+        scaleUp = scaleUp - .01;
+			TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+    } else if (e.scale > 1.0) {
+        scaleUp = scaleUp + .01;
+    	TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+    }
+}, false);   
 
-
-        //pinch
-        if (ev.type == "pinch") {
-            scale = Math.max(.999, Math.min(last_scale * (ev.scale), 4));
-        }
-        if(ev.type == "pinchend"){last_scale = scale;}
-
-        //panend
-        if(ev.type == "panend"){
-            last_posX = posX < max_pos_x ? posX : max_pos_x;
-            last_posY = posY < max_pos_y ? posY : max_pos_y;
-        }
-
-        if (scale != 1) {
-            transform =
-                "translate3d(" + posX + "px," + posY + "px, 0) " +
-                "scale3d(" + scale + ", " + scale + ", 1)";
-        }
-
-        if (transform) {
-            el.style.webkitTransform = transform;
-        }
-    });
+var scaleUp = 1;
+function zoomSchematic(e){
+	e.preventDefault();
+	switch(e.detail>0 && scaleUp>1) {
+		case true:
+		if(scaleUp > .5 ){
+			scaleUp = scaleUp - .1;
+			TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+		}
+		break;
+		case false:
+		scaleUp = scaleUp + .1;
+    	TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+        break;
+    }
 }
-
-
-
-// schematic.addEventListener('gesturechange', function(e) {
-// 	e.preventDefault();
-//     if (e.scale < 1.0 && scaleUp > 1) {
-//         scaleUp = scaleUp - .01;
-// 			TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-//     } else if (e.scale > 1.0) {
-//         scaleUp = scaleUp + .01;
-//     	TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-//     }
-// }, false);   
-
-// var scaleUp = 1;
-// function zoomSchematic(e){
-// 	e.preventDefault();
-// 	switch(e.detail>0 && scaleUp>1) {
-// 		case true:
-// 		if(scaleUp > .5 ){
-// 			scaleUp = scaleUp - .25;
-// 			TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-// 		}
-// 		break;
-// 		case false:
-// 		scaleUp = scaleUp + .25;
-//     	TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-//         break;
-//     }
-// }
 
 function componentChange(){
 	display = document.getElementById("componentSelect").value;
