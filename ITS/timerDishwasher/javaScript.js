3b04db627077d86fcd22ea61810373e6e86a40b6
robert may <robert.may@interactivetechnicalsolutions.com>
2019-11-16 11:12:00 -0500

    J

--- ITS/timerDishwasher/javaScript.js
+++ ITS/timerDishwasher/javaScript.js
@@ -1,4 +1,4 @@
-document.title = "Dishwasher Timer Model"
+document.title = "Dishwasher - Timer Model"
 
 var deviceType = "not mobile";
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
