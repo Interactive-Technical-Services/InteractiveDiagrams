51127c3cdef3548b508b66e41ce978ef0eca7914
robert may <robert.may@interactivetechnicalsolutions.com>
2020-10-27 23:44:17 -0400

    J

--- bibleScript.js
+++ bibleScript.js
@@ -14,6 +14,7 @@ xhttp.onload = function () {
 	
 	populateChapterDD();
 	chapters = dataDiv.getElementsByTagName("div");
+
 	for(i=0; i<chapters.length; i++){
 		paragraphs = chapters[i].getElementsByTagName("p");
 		spans = chapters[i].getElementsByTagName("span");
@@ -126,204 +127,271 @@ function mySearchInputFunction(){
 function showAnchor(id){
 	idSplit = id.split(":")
 	book = idSplit[0].split(" ")
+	chapterDD.selectedIndex = parseInt(book[1]-1);
 	switch(book[0]) {
  		case "Gen":
  		  bookNav = "Genesis"+ book[1];
+ 		  bookDD.selectedIndex  = 0;
  		  break;
  		case "Exo":
  		  bookNav = "Exodus"+ book[1];
+ 		  bookDD.selectedIndex  = 1;
  		  break;
  		  case "Lev":
  		  bookNav = "Leviticus"+ book[1];
+ 		  bookDD.selectedIndex  = 2;
  		  break;
  		  case "Num":
  		  bookNav = "Numbers"+ book[1];
+ 		  bookDD.selectedIndex  = 3;
  		  break;
  		  case "Deu":
  		  bookNav = "Deuteronomy"+ book[1];
+ 		  bookDD.selectedIndex  = 4;
  		  break;
  		  case "Jos":
  		  bookNav = "Joshua"+ book[1];
+ 		  bookDD.selectedIndex  = 5;
  		  break;
  		  case "Jdg":
  		  bookNav = "Judges"+ book[1];
+ 		  bookDD.selectedIndex  = 6;
  		  break;
  		  case "Rth":
  		  bookNav = "Ruth"+ book[1];
+ 		  bookDD.selectedIndex  = 7;
  		  break;
  		  case "1Sa":
  		  bookNav = "1 Samuel"+ book[1];
+ 		  bookDD.selectedIndex  = 8;
  		  break;
  		  case "2Sa":
  		  bookNav = "2 Samuel"+ book[1];
+ 		  bookDD.selectedIndex  = 9;
  		  break;
  		  case "1Ki":
  		  bookNav = "1 Kings"+ book[1];
+ 		  bookDD.selectedIndex  = 10;
  		  break;
  		  case "2Ki":
  		  bookNav = "2 Kings"+ book[1];
+ 		  bookDD.selectedIndex  = 11;
  		  break;
  		  case "1Ch":
  		  bookNav = "1 Chronicles"+ book[1];
+ 		   bookDD.selectedIndex  = 12;
  		  break;
  		  case "2Ch":
  		  bookNav = "2 Chronicles"+ book[1];
+ 		   bookDD.selectedIndex  = 13;
  		  break;
  		  case "Ezr":
  		  bookNav = "Ezra"+ book[1];
+ 		   bookDD.selectedIndex  = 14;
  		  break;
  		  case "Neh":
  		  bookNav = "Nehemiah"+ book[1];
+ 		   bookDD.selectedIndex  = 15;
  		  break;
  		  case "Est":
  		  bookNav = "Esther"+ book[1];
+ 		   bookDD.selectedIndex  = 16;
  		  break;
  		  case "Job":
  		  bookNav = "Job"+ book[1];
+ 		   bookDD.selectedIndex  = 17;
  		  break;
  		  case "Psa":
  		  bookNav = "Psalms"+ book[1];
+ 		   bookDD.selectedIndex  = 18;
  		  break;
  		  case "Pro":
  		  bookNav = "Proverbs"+ book[1];
+ 		   bookDD.selectedIndex  = 19;
  		  break;
  		  case "Ecc":
  		  bookNav = "Ecclesiastes"+ book[1];
+ 		   bookDD.selectedIndex  = 20;
  		  break;
  		  case "Son":
  		  bookNav = "Song of Solomon"+ book[1];
+ 		  bookDD.selectedIndex  = 21;
  		  break;
  		  case "Isa":
  		  bookNav = "Isaiah"+ book[1];
+ 		  bookDD.selectedIndex  = 22;
  		  break;
  		  case "Jer":
  		  bookNav = "Jeremiah"+ book[1];
+ 		  bookDD.selectedIndex  = 23;
  		  break;
  		  case "Lam":
  		  bookNav = "Lamentations"+ book[1];
+ 		  bookDD.selectedIndex  = 24;
  		  break;
  		  case "Eze":
  		  bookNav = "Ezekiel"+ book[1];
+ 		  bookDD.selectedIndex  = 25;
  		  break;
  		  case "Dan":
  		  bookNav = "Daniel"+ book[1];
+ 		  bookDD.selectedIndex  = 26;
  		  break;
  		  case "Hos":
  		  bookNav = "Hosea"+ book[1];
+ 		  bookDD.selectedIndex  = 27;
  		  break;
  		  case "Joe":
  		  bookNav = "Joel"+ book[1];
+ 		  bookDD.selectedIndex  = 28;
  		  break;
  		  case "Amo":
  		  bookNav = "Amos"+ book[1];
+ 		  bookDD.selectedIndex  = 29;
  		  break;
  		  case "Oba":
  		  bookNav = "Obadiah"+ book[1];
+ 		  bookDD.selectedIndex  = 30;
  		  break;
  		  case "Jon":
  		  bookNav = "Jonah"+ book[1];
+ 		  bookDD.selectedIndex  = 31;
  		  break;
  		  case "Mic":
  		  bookNav = "Micah"+ book[1];
+ 		  bookDD.selectedIndex  = 32;
  		  break;
  		  case "Nah":
  		  bookNav = "Nahum"+ book[1];
+ 		  bookDD.selectedIndex  = 33;
  		  break;
  		  case "Hab":
  		  bookNav = "Habakkuk"+ book[1];
+ 		  bookDD.selectedIndex  = 34;
  		  break;
  		  case "Zep":
  		  bookNav = "Zephaniah"+ book[1];
+ 		  bookDD.selectedIndex  = 35;
  		  break;
  		  case "Hag":
  		  bookNav = "Haggai"+ book[1];
+ 		  bookDD.selectedIndex  = 36;
  		  break;
  		  case "Zec":
  		  bookNav = "Zechariah"+ book[1];
+ 		  bookDD.selectedIndex  = 37;
  		  break;
  		  case "Mal":
  		  bookNav = "Malachi"+ book[1];
+ 		  bookDD.selectedIndex  = 38;
  		  break;
  		  case "Mat":
  		  bookNav = "Matthew"+ book[1];
+ 		  bookDD.selectedIndex  = 39;
  		  break;
  		  case "Mar":
  		  bookNav = "Mark"+ book[1];
+ 		  bookDD.selectedIndex  = 40;
  		  break;
  		  case "Luk":
  		  bookNav = "Luke"+ book[1];
+ 		  bookDD.selectedIndex  = 41;
  		  break;
  		  case "Joh":
  		  bookNav = "John"+ book[1];
+ 		  bookDD.selectedIndex  = 42;
  		  break;
  		  case "Act":
  		  bookNav = "Acts"+ book[1];
+ 		  bookDD.selectedIndex  = 43;
  		  break;
  		  case "Rom":
  		  bookNav = "Romans"+ book[1];
+ 		  bookDD.selectedIndex  = 44;
  		  break;
  		  case "1Co":
  		  bookNav = "1 Corinthians"+ book[1];
+ 		  bookDD.selectedIndex  = 45;
  		  break;
  		  case "2Co":
  		  bookNav = "2 Corinthians"+ book[1];
+ 		  bookDD.selectedIndex  = 46;
  		  break;
  		  case "Gal":
  		  bookNav = "Galatians"+ book[1];
+ 		  bookDD.selectedIndex  = 47;
  		  break;
  		  case "Eph":
  		  bookNav = "Ephesians"+ book[1];
+ 		  bookDD.selectedIndex  = 48;
  		  break;
  		  case "Php":
  		  bookNav = "Philippians"+ book[1];
+ 		  bookDD.selectedIndex  = 49;
  		  break;
  		  case "Col":
  		  bookNav = "Colossians"+ book[1];
+ 		  bookDD.selectedIndex  = 50;
  		  break;
  		  case "1Th":
  		  bookNav = "1 Thessalonians"+ book[1];
+ 		  bookDD.selectedIndex  = 51;
  		  break;
  		  case "2Th":
  		  bookNav = "2 Thessalonians"+ book[1];
+ 		  bookDD.selectedIndex  = 52;
  		  break;
  		  case "1Ti":
  		  bookNav = "1 Timothy"+ book[1];
+ 		  bookDD.selectedIndex  = 53;
  		  break;
  		  case "2Ti":
  		  bookNav = "2 Timothy"+ book[1];
+ 		  bookDD.selectedIndex  = 54;
  		  break;
  		  case "Tit":
  		  bookNav = "Titus"+ book[1];
+ 		  bookDD.selectedIndex  = 55;
  		  break;
  		  case "Phm":
  		  bookNav = "Philemon"+ book[1];
+ 		  bookDD.selectedIndex  = 56;
  		  break;
  		  case "Heb":
  		  bookNav = "Hebrews"+ book[1];
+ 		  bookDD.selectedIndex  = 57;
  		  break;
  		  case "Jas":
  		  bookNav = "James"+ book[1];
+ 		  bookDD.selectedIndex  = 58;
  		  break;
  		  case "1Pe":
  		  bookNav = "1 Peter"+ book[1];
+ 		  bookDD.selectedIndex  = 59;
  		  break;
  		  case "2Pe":
  		  bookNav = "2 Peter"+ book[1];
+ 		  bookDD.selectedIndex  = 60;
  		  break;
  		  case "1Jn":
  		  bookNav = "1 John"+ book[1];
+ 		  bookDD.selectedIndex  = 61;
  		  break;
  		  case "2Jn":
  		  bookNav = "2 John"+ book[1];
+ 		  bookDD.selectedIndex  = 62;
  		  break;
  		  case "3Jn":
  		  bookNav = "3 John"+ book[1];
+ 		  bookDD.selectedIndex  = 63;
  		  break;
  		  case "Jud":
  		  bookNav = "Jude"+ book[1];
+ 		  bookDD.selectedIndex  = 64;
  		  break;
  		  case "Rev":
  		  bookNav = "Revelation"+ book[1];
+ 		  bookDD.selectedIndex  = 65;
  		  break;
 	}
 	searchDiv.style.display="none";
@@ -338,34 +406,6 @@ function prePop(e){
 
 myInput.value = prePopDD.value;
 
-
-
-// function setUpLinks(){
-
-// 	for(a=0; a<hyperLinks.length; a++){
-// 		var searchValue = hyperLinks[a];
-// 		var searchRegex = new RegExp(searchValue, "gi");
-// 		for(i=0; i<verses.length; i++){
-// 			var verseHTML = verses[i].innerHTML;
-// 			if(verseHTML.match(searchRegex) != null){
-// 				var str = document.getElementById(verses[i].id).outerHTML
-				
-//   				var stringMatch = str.match(searchRegex);
-
-//   				var res = str.replace(searchRegex, "<a href='Phrases/" + searchValue + ".html' target='_blank'>" + str.match(searchRegex)[0] + "</a>");
-//   				console.log(res)
-//   				document.getElementById(verses[i].id).innerHTML = res
-//   				// verses[i].innerHTML = res;
-//   				// console.log(verses[i].innerHTML)
-//   			}
-//   		}
-// 	}
-// }
-
-
-
-// var str = "This is some HTML text <a href='Phrases/Jesus Christ.html' target='_blank'>programming</a>";
-// console.log(str.replace(str.match(/<a(.*?)<\/a>/g), "written by Robert May."))
 var hyperLinks = ["third day", "summer fruit", "ten thousand"];
 
 function setUpLinks(){
@@ -384,6 +424,10 @@ function setUpLinks(){
 		}
 	}
 	bibleDiv.innerHTML = chapVerse.innerHTML;
+}
+
+function textLinkClicked(){
+
 }
 
 
@@ -439,10 +483,93 @@ function wordDefinition(source){
 
 ////Next Navigation
 function goBack(){
-	console.log("go back")
+	if(chapterDD.selectedIndex > -1){
+		chapterDD.selectedIndex--
+	}
+	if(chapterDD.selectedIndex === -1 && bookDD.selectedIndex > 0){
+		bookDD.selectedIndex--
+		length =parseInt(bookDD.options[bookDD.selectedIndex].value);
+		chapterDD.options.length = 0;
+		for (var i = 0; i < length; i++) {
+			console.log('fired')
+    	    var option = document.createElement("OPTION");
+    	    option.innerHTML = i+1;
+    	    chapterDD.options.add(option)
+    	} 
+		console.log(bookDD.value)
+		chapterDD.selectedIndex = bookDD.value -1
+	}
+	chapVerse = (bookDD.options[bookDD.selectedIndex].text + [chapterDD.selectedIndex + 1]);
+	chapVerse=document.getElementById(chapVerse);
+	bibleDiv.innerHTML = chapVerse.innerHTML;
 }
 
 function goForward(){
-	console.log("go forward")
+	if(chapterDD.selectedIndex < chapterDD.length-1){
+		chapterDD.selectedIndex++
+		console.log(chapterDD.selectedIndex)
+	}else{
+		if(chapterDD.selectedIndex === chapterDD.length-1 && bookDD.selectedIndex < bookDD.length){
+		bookDD.selectedIndex++
+		chapterDD.selectedIndex = 0
+		length =parseInt(bookDD.options[bookDD.selectedIndex].value);
+		chapterDD.options.length = 0;
+		for (var i = 0; i < length; i++) {
+			console.log('fired')
+    	    var option = document.createElement("OPTION");
+    	    option.innerHTML = i+1;
+    	    chapterDD.options.add(option)
+    	} 
+		console.log(bookDD.value)
+	}
+	}
+	
+	chapVerse = (bookDD.options[bookDD.selectedIndex].text + [chapterDD.selectedIndex + 1]);
+	chapVerse=document.getElementById(chapVerse);
+	bibleDiv.innerHTML = chapVerse.innerHTML;
+}
+
+function detectswipe(el,func) {
+  swipe_det = new Object();
+  swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+  var min_x = 30;  //min x swipe for horizontal swipe
+  var max_x = 30;  //max x difference for vertical swipe
+  var min_y = 50;  //min y swipe for vertical swipe
+  var max_y = 60;  //max y difference for horizontal swipe
+  var direc = "";
+  ele = document.getElementById(el);
+  ele.addEventListener('touchstart',function(e){
+    var t = e.touches[0];
+    swipe_det.sX = t.screenX; 
+    swipe_det.sY = t.screenY;
+  },false);
+  ele.addEventListener('touchmove',function(e){
+    e.preventDefault();
+    var t = e.touches[0];
+    swipe_det.eX = t.screenX; 
+    swipe_det.eY = t.screenY;    
+  },false);
+  ele.addEventListener('touchend',function(e){
+    //horizontal detection
+    if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+      if(swipe_det.eX > swipe_det.sX) direc = "r";
+      else direc = "l";
+    }
+    //vertical detection
+    else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+      if(swipe_det.eY > swipe_det.sY) direc = "d";
+      else direc = "u";
+    }
+
+    if (direc != "") {
+      if(typeof func == 'function') func(el,direc);
+    }
+    direc = "";
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+  },false);  
+}
+
+function myfunction(el,d) {
+  alert("you swiped on element with id '"+el+"' to "+d+" direction");
 }
 
